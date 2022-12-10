c5e7f5cb89ee99e9ce545ef3340ac9aa188b28a3
cwtu <cwtu12@gmail.com>
2022-12-10 12:22:12 -0500

    booklist, addbBook, removeBook

--- myBookshelfServer.js
+++ myBookshelfServer.js
@@ -15,6 +15,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+// show users booklist
+app.get("/booklist", (request, response) => {
+    
+    getBooklist().then(books => response.render("booklist", books));
+});
+
+// add books to booklist
+app.post("/addBook", (request, response) => {
+    const bookInfo = {
+        key: request.body.key,
+        title: request.body.title,
+        authors: request.body.authors,
+        description: request.body.description,
+    }
+
+    addBook(bookInfo);
+});
+
+// remove a book from booklist and go back to booklist page
+app.post("/removeBook", (request, response) => {
+  const variables = {
+    key: request.body.key,
+  };
+
+  removeBook(key).then(() => response.reneder("booklist"));
+});
+
 
 process.stdin.setEncoding("utf8");
 
@@ -22,10 +52,6 @@ if (process.argv.length != 3) {
     process.stdout.write("Usage myBookshelfServer.js PORT_NUMBER");
     process.exit(1);
 }
-
-app.set("views", path.resolve(__dirname, "templates"));
-app.set("view engine", "ejs");
-
 const portNumber = process.argv[2];
 
 
@@ -47,4 +73,60 @@ process.stdin.on("readable", function(){
         process.stdout.write(prompt);
         process.stdin.resume();
     }
-})
+});
+
+async function getBooklist() {
+    try {
+        await client.connect();
+        const result = await client
+            .db(databaseAndCollection.db)
+            .collection(databaseAndCollection.collection)
+            .findOne({"_id": userId});
+
+        await client.close();
+        return result.booklist;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function addBook(bookInfo) {
+    try {
+        await client.connect();
+        const result = await client
+            .db(databaseAndCollection.db)
+            .collection(databaseAndCollection.collection)
+            .updateOne(
+                {"_id": userId},
+                {
+                    $push: {
+                        booklist: bookInfo
+                }});
+
+        await client.close();
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function removeBook(key) {
+  try {
+    await client.connect();
+    const result = await client
+      .db(databaseAndCollection.db)
+      .collection(databaseAndCollection.collection)
+      .updateOne(
+        { _id: userId },
+        {
+          $pull: {
+            booklist: {key: key},
+          },
+        }
+        );
+    await client.close();
+
+  } catch (e) {
+    console.log(e);
+  }
+}
