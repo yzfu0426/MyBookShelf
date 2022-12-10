fe5e4730131b8c43c085376f753f560c32f6e136
yzfu0426 <fyz04262000@gmail.com>
2022-12-09 23:40:27 -0500

    User Sign Up and User Login

--- myBookshelfServer.js
+++ myBookshelfServer.js
@@ -10,10 +10,11 @@ const collection = process.env.MONGO_COLLECTION;
 const databaseAndCollection = {db: db, collection: collection};
 
 const { MongoClient, ServerApiVersion} = require('mongodb');
-const uri = `mongodb+srv://${userName}:${password}@cluster0.vj5zf11.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${userName}:${password}@cluster0.gyq2d5s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 const bodyParser = require("body-parser");
+const { request } = require('http');
+const { response } = require('express');
 app.use(bodyParser.urlencoded({extended:false}));
 
 process.stdin.setEncoding("utf8");
@@ -27,6 +28,78 @@ app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
 
 const portNumber = process.argv[2];
+let buttonName1, buttonName2;
+
+app.get('/', (request, response) => {
+    // let buttonName1 = loggedIn ? "Booklist" : "Log in";
+    // let buttonName2 = loggedIn ? user : "Sign Up";
+    buttonName1 = "Login";
+    buttonName2 = "Sign up";
+    response.render('index', {buttonName1, buttonName2});
+})
+
+// TODO: add password double confirmation, check whether user already existed in the database
+app.get('/signUp', (request, response) => {
+    let title = "Sign Up";
+    let process = "processSignUp";
+    response.render('credentials', {portNumber, process, title});
+})
+
+app.post('/processSignUp', async (request, response) => {
+    let {username, password} = request.body;
+    try {
+        await client.connect();
+        console.log("**** Inserting one user ****");
+        let user = {username: username, 
+                    password: password,
+                    booklist: []};
+        await insertUser(user);
+        buttonName1 = "Booklist";
+        buttonName2 = username;
+        response.render('index', {buttonName1, buttonName2});
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+})
+
+app.get('/logIn', (request, response) => {
+    let title = "Log In";
+    let process = "processLogIn";
+    response.render('credentials', {portNumber, process, title});
+})
+
+app.post('/processLogIn', async (request, response) => {
+    let {username, password} = request.body;
+    try {
+        await client.connect();
+        console.log("**** Log In one user ****");
+        let user = await logUserIn(username, password);
+        // TODO: add logic to check whether user exists
+        if (user['password'] !== password) {
+            console.log("Incorrect password, try again");
+            let title = "Log In";
+            let process = "processLogIn";
+            response.render('credentials', {portNumber, process, title});
+        } else {
+            buttonName1 = "Booklist";
+            buttonName2 = username;
+            response.render('index', {buttonName1, buttonName2});
+        }
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+})
+
+
+//https://openlibrary.org/search.json?title=atomic+habits
+app.get('/search', (request, response) => {
+    const {title} = request.query;
+})
+
 
 
 app.listen(portNumber);
@@ -48,3 +121,24 @@ process.stdin.on("readable", function(){
         process.stdin.resume();
     }
 })
+
+
+
+async function insertUser(user) {
+    const result = await client
+                        .db(databaseAndCollection.db)
+                        .collection(databaseAndCollection.collection)
+                        .insertOne(user);
+    console.log(`Applicant entry created with id ${result.insertedId}`);
+}
+
+
+async function logUserIn(username, password) {
+    let filter = {username:username};
+    const result = await client.db(databaseAndCollection.db)
+                         .collection(databaseAndCollection.collection)
+                         .findOne(filter);
+
+    return result;
+
+} 
